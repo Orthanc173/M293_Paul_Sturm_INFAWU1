61ca5b6773b004d3c5b9997a46551f44b55fdb74
Orthanc173 <paul.sturm@lernende.bildxzug.ch>
2026-09-11 15:15:39 +0200

    added JS dynamic items

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,7 +12,7 @@ const message = document.querySelector("#message");
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (event) => {
+form?.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(event);
 
@@ -28,3 +28,47 @@ form.addEventListener("submit", (event) => {
 
   alert(`Danke für's Einsenden deiner Nachricht, ${nameContent}! 😘👌`);
 });
+
+/*
+API: dynamische Produkte
+*/
+
+const ulProducts = document.querySelector("ul.contentList");
+
+const getData = async () => {
+  const result = await fetch(
+    "https://dummyjson.com/products/category/smartphones",
+  );
+  const data = await result.json();
+  const smartphones = data["products"];
+  console.log(smartphones);
+
+  for (let i = 0; i <= smartphones.length; i++) {
+    ulProducts.innerHTML += `
+    <li class="contentItem">
+        <img src="${smartphones[i].thumbnail}" alt="Produktbild von ${smartphones[i].title}" /><br />
+        <div class="itemInfo">
+          <h2>${smartphones[i].title}</h2>
+          <div class="purchaseInfo">
+            <h2 class="price">${smartphones[i].price} CHF</h2>
+            <img
+              class="warenkorb"
+              src="../assets/cart.png"
+              alt="Warenkorb Favicon"
+            />
+          </div>
+        </div>
+        <select>
+          <option value="">Speicherplatz auswählen</option>
+          <option value="16">16GB</option>
+          <option value="32">32GB</option>
+          <option value="64">64GB</option>
+        </select>
+        <p>
+          ${smartphones[i].description}
+        </p>
+      </li>`;
+  }
+};
+
+getData();
